4552c6647b3bd396284393f8745db8b398d314b4
TED Vortex <ted.vortex@gmail.com>
2022-08-05 20:03:47 +0200

    feat: add get repository by id and full name

--- src/repo/repo.controller.ts
+++ src/repo/repo.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, HttpCode, HttpStatus, Query } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, NotFoundException, Param, Query } from "@nestjs/common";
 import { RepoService } from "./repo.service";
 import { Repo } from "./repo.entity";
-import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { PageOptionsDto } from "../common/dtos/page-options.dto";
 import { PageDto } from "../common/dtos/page.dto";
 import { ApiPaginatedResponse } from "../common/decorators/api-paginated-response.decorator";
@@ -10,6 +10,35 @@ import { ApiPaginatedResponse } from "../common/decorators/api-paginated-respons
 @ApiTags("Repositories")
 export class RepoController {
   constructor(private readonly repoService: RepoService) {}
+
+  @Get("/:id")
+  @ApiOperation({
+    operationId: "findOneById",
+    summary: "Finds a repo by :id",
+  })
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: Repo })
+  @ApiNotFoundResponse({ type: NotFoundException })
+  async findOneById(
+    @Param("id") id: number,
+  ): Promise<Repo> {
+    return this.repoService.findOneById(id);
+  }
+
+  @Get("/:owner/:repo")
+  @ApiOperation({
+    operationId: "findOneByOwnerAndRepo",
+    summary: "Finds a repo by :owner and :repo",
+  })
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ type: Repo })
+  @ApiNotFoundResponse({ type: NotFoundException })
+  async findOneByOwnerAndRepo(
+    @Param("owner") owner: string,
+      @Param("repo") repo: string,
+  ): Promise<Repo> {
+    return this.repoService.findOneByOwnerAndRepo(owner, repo);
+  }
 
   @Get("/list")
   @HttpCode(HttpStatus.OK)
