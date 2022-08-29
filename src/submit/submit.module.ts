09c96fea5b68eb80d14b40b7e6260d1c427ca4f9
TED Vortex <ted.vortex@gmail.com>
2022-08-29 03:35:40 +0200

    feat: add submit repo authenticated feature
    closes #21

---
+++ src/submit/submit.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { DbRepo } from "../repo/entities/repo.entity";
+import { DbRepoToUserSubmissions } from "../repo/entities/repo.to.user.submissions.entity";
+import { RepoService } from "../repo/repo.service";
+import { SubmitService } from "./submit.service";
+import { RepoSubmitController } from "./repo-submit.controller";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([
+    DbRepo,
+    DbRepoToUserSubmissions
+  ])],
+  controllers: [RepoSubmitController],
+  providers: [RepoService, SubmitService],
+  exports: [SubmitService],
+})
+export class SubmitModule {}
