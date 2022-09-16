604a19269e62adf8b0d17ccfc3c216c448784c10
Brandon Roberts <robertsbt@gmail.com>
2022-09-16 09:42:58 -0500

    feat: add endpoint to retrieve onboarding repos based on the PAT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, NotFoundException, Post, UseGuards } from "@nestjs/common";
 import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { execSync } from "child_process";
 import { SupabaseGuard } from "./supabase.guard";
 import { SupabaseAuthUser } from "nestjs-supabase-auth";
 import { User } from "./supabase.user.decorator";
@@ -70,5 +71,33 @@ export class AuthController {
     }
 
     return user;
+  }
+
+  @Post("/onboarding/repos")
+  @ApiBearerAuth()
+  @UseGuards(SupabaseGuard)
+  @ApiOperation({
+    operationId: "postOnboardingRepos",
+    summary: "Retrieves onboarding repos using a PAT",
+  })
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  postOnboardingRepos (
+    @Body() body: string,
+  ): string[] {
+    const { token } = JSON.parse(body || "{}") as { token: string | undefined };
+
+    if (!token) {
+      throw (new NotFoundException);
+    }
+
+    try {
+      const repoList = execSync(`npx repo-report ls --token="${token}"`).toString("utf-8");
+      const repos = repoList.split(/\n/).filter(repo => repo !== "");
+
+      return repos;
+    } catch (e) {
+      throw (new NotFoundException);
+    }
   }
 }
