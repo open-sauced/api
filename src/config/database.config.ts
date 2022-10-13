46b10dae6db026d6f620a5c1ccb42644f267588a
TED Vortex <ted.vortex@gmail.com>
2022-10-13 02:37:47 +0200

    refactor: improve sql logging and slow query timeout

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -7,4 +7,5 @@ export default registerAs("db", () => ({
   username: String(process.env.TYPEORM_USERNAME ?? "ospz-local"),
   password: String(process.env.TYPEORM_PASSWORD ?? "UCN2zrH2WGxKck7tT2JG4MY6wbHkeX9s"),
   database: String(process.env.TYPEORM_DATABASE ?? "ospz-local"),
+  maxQueryExecutionTime: Number(parseInt(process.env.TYPEORM_MAX_QUERY_EXECUTION_TIME ?? "2000", 10)),
 }));
