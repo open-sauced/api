e1d01fd2fe9b5cf15ce1d66eec58af72b9ed9ba4
0-vortex <0-vortex@users.noreply.github.com>
2022-10-13 00:57:22 +0000

    style: remove extraneous console.logs e3d7bd71471c0c56e57372cbd0477aa9d351f270

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -377,6 +377,9 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="classes/ContributionPageOptionsDto.html" data-type="entity-link" >ContributionPageOptionsDto</a>
                             </li>
                             <li class="link">
+                                <a href="classes/DatabaseLoggerMiddleware.html" data-type="entity-link" >DatabaseLoggerMiddleware</a>
+                            </li>
+                            <li class="link">
                                 <a href="classes/PageDto.html" data-type="entity-link" >PageDto</a>
                             </li>
                             <li class="link">
@@ -393,6 +396,19 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             </li>
                         </ul>
                     </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#injectables-links"' :
+                                'data-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/HttpLoggerMiddleware.html" data-type="entity-link" >HttpLoggerMiddleware</a>
+                                </li>
+                            </ul>
+                        </li>
                     <li class="chapter">
                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#interfaces-links"' :
                             'data-target="#xs-interfaces-links"' }>
