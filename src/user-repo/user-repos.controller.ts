97a6882b29ef50e940b8291a7533aeb4f081077e
Brandon Roberts <robertsbt@gmail.com>
2022-11-01 09:55:07 -0500

    feat: add user repos list

--- src/user-repo/user-repos.controller.ts
+++ src/user-repo/user-repos.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, NotFoundException, Post, UseGuards } from "@nestjs/common";
-import { ApiOperation, ApiNotFoundResponse, ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, NotFoundException, Post, Query, UseGuards } from "@nestjs/common";
+import { ApiOperation, ApiNotFoundResponse, ApiBearerAuth, ApiTags, ApiOkResponse } from "@nestjs/swagger";
 
-import { SupabaseGuard } from "../auth/supabase.guard";
+import { ApiPaginatedResponse } from "../common/decorators/api-paginated-response.decorator";
+import { PageDto } from "../common/dtos/page.dto";
 import { UserId } from "../auth/supabase.user.decorator";
+import { SupabaseGuard } from "../auth/supabase.guard";
+
+import { UserRepoOptionsDto } from "./dtos/user-repo-options.dto";
+import { DbUserRepo } from "./user-repo.entity";
 
 import { UserReposService } from "./user-repos.service";
 
@@ -12,6 +17,23 @@ export class UserReposController {
   constructor (
     private readonly userReposService: UserReposService,
   ) {}
+
+  @Get("/repos")
+  @ApiOperation({
+    operationId: "findAllReposByUserId",
+    summary: "Listing all repos for the authenticated user and paginate them",
+  })
+  @ApiBearerAuth()
+  @UseGuards(SupabaseGuard)
+  @ApiPaginatedResponse(DbUserRepo)
+  @ApiOkResponse({ type: DbUserRepo })
+  @ApiNotFoundResponse({ description: "User repos not found" })
+  async findAllReposByUserId (
+    @Query() pageOptionsDto: UserRepoOptionsDto,
+      @UserId() userId: string,
+  ): Promise<PageDto<DbUserRepo>> {
+    return this.userReposService.findAllByUserId(pageOptionsDto, userId);
+  }
 
   @Post("/repos")
   @ApiOperation({
