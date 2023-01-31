a51cbb9e3d4017d091b024e8c6bfdc6dd7a28eb3
brandonroberts <brandonroberts@users.noreply.github.com>
2023-01-31 15:11:49 +0000

    feat: add endpoint to update user email preferences (#90) 8c6884c2f2c2e8d81831ce16aef50801793304c6

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -558,6 +558,12 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="classes/UpdateUserDto.html" data-type="entity-link" >UpdateUserDto</a>
                             </li>
                             <li class="link">
+                                <a href="classes/UpdateUserEmailPreferencesDto.html" data-type="entity-link" >UpdateUserEmailPreferencesDto</a>
+                            </li>
+                            <li class="link">
+                                <a href="classes/UpdateUserProfileInterestsDto.html" data-type="entity-link" >UpdateUserProfileInterestsDto</a>
+                            </li>
+                            <li class="link">
                                 <a href="classes/UserRepoOptionsDto.html" data-type="entity-link" >UserRepoOptionsDto</a>
                             </li>
                         </ul>
