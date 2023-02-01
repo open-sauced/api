c8dab921a3b5b89244ffd6ecf1bdf4e93b6d15ba
TED Vortex <ted.vortex@gmail.com>
2023-02-01 02:06:33 +0200

    refactor: move migrations to digital ocean structure and prepare for pgmigrate

--- migrations/20221221124600_add_customers.sql
+++ migrations/20221221124600_add_customers.sql
@@ -1,23 +1,8 @@
--- Table: public.customers
-
--- DROP TABLE IF EXISTS public.customers;
-
-CREATE TABLE IF NOT EXISTS public.customers
+create table if not exists public.customers
 (
-    id bigint NOT NULL,
-    stripe_customer_id text COLLATE pg_catalog."default",
-    CONSTRAINT customers_pkey PRIMARY KEY (id)
+    id bigint not null,
+    stripe_customer_id text collate pg_catalog."default",
+    constraint customers_pkey primary key (id)
 )
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.customers
-    OWNER to postgres;
-
-GRANT ALL ON TABLE public.customers TO anon;
-
-GRANT ALL ON TABLE public.customers TO authenticated;
-
-GRANT ALL ON TABLE public.customers TO postgres;
-
-GRANT ALL ON TABLE public.customers TO service_role;
+tablespace pg_default;
--- migrations/20221221124800_add_subscriptions.sql
+++ migrations/20221221124800_add_subscriptions.sql
@@ -1,36 +1,21 @@
--- Table: public.subscriptions
-
--- DROP TABLE IF EXISTS public.subscriptions;
-
-CREATE TABLE IF NOT EXISTS public.subscriptions
+create table if not exists public.subscriptions
 (
-    id text COLLATE pg_catalog."default" NOT NULL,
-    user_id bigint NOT NULL,
-    status subscription_status,
+    id text collate pg_catalog."default" not null,
+    user_id bigint not null,
+    status varchar(255) collate pg_catalog."default" not null default 'trialing'::character varying,
     metadata jsonb,
-    price_id text COLLATE pg_catalog."default",
+    price_id text collate pg_catalog."default",
     quantity integer,
     cancel_at_period_end boolean,
-    created_at timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
-    current_period_start_at timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
-    current_period_end_at timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
+    created_at timestamp with time zone not null default timezone('utc'::text, now()),
+    current_period_start_at timestamp with time zone not null default timezone('utc'::text, now()),
+    current_period_end_at timestamp with time zone not null default timezone('utc'::text, now()),
     ended_at timestamp with time zone,
     cancel_at timestamp with time zone,
     canceled_at timestamp with time zone,
     trial_start_at timestamp with time zone,
     trial_end_at timestamp with time zone,
-    CONSTRAINT subscriptions_pkey PRIMARY KEY (id)
+    constraint subscriptions_pkey primary key (id)
 )
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.subscriptions
-    OWNER to postgres;
-
-GRANT ALL ON TABLE public.subscriptions TO anon;
-
-GRANT ALL ON TABLE public.subscriptions TO authenticated;
-
-GRANT ALL ON TABLE public.subscriptions TO postgres;
-
-GRANT ALL ON TABLE public.subscriptions TO service_role;
+tablespace pg_default;
