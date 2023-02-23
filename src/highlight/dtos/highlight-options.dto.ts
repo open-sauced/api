d5f62b198a2df0a8b59f2995f44d75b41aa9c4e7
Brandon Roberts <robertsbt@gmail.com>
2023-02-23 14:40:25 -0600

    chore: added repo filter example

--- src/highlight/dtos/highlight-options.dto.ts
+++ src/highlight/dtos/highlight-options.dto.ts
@@ -3,7 +3,10 @@ import { IsOptional, IsString } from "class-validator";
 import { PageOptionsDto } from "../../common/dtos/page-options.dto";
 
 export class HighlightOptionsDto extends PageOptionsDto {
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: "Highlight Repo Filter",
+    example: "open-sauced/insights"
+  })
   @IsString()
   @IsOptional()
   readonly repo?: string;
