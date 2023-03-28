ecf4382100360f75be88ab4bfa79493b8a109821
Brandon Roberts <robertsbt@gmail.com>
2023-03-28 09:25:53 -0500

    feat: add endpoints for managing user follows

---
+++ src/user/entities/user-follows.entity.ts
@@ -0,0 +1,60 @@
+import {
+  Column,
+  CreateDateColumn, DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { ApiHideProperty } from "@nestjs/swagger";
+import { ApiModelProperty, ApiModelPropertyOptional } from "@nestjs/swagger/dist/decorators/api-model-property.decorator";
+
+@Entity({ name: "users_to_users_followers" })
+export class DbUserToUserFollows {
+  @ApiModelProperty({
+    description: "User Follow identifier",
+    example: 196,
+  })
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @ApiModelProperty({
+    description: "User identifier",
+    example: 237133,
+  })
+  @Column()
+  public user_id!: number;
+
+  @ApiModelProperty({
+    description: "User follower identifier",
+    example: 71359796,
+  })
+  @Column()
+  public following_user_id!: number;
+
+  @ApiModelPropertyOptional({
+    description: "Timestamp representing user follow creation",
+    example: "2016-10-19 13:24:51.000000",
+  })
+  @CreateDateColumn({
+    type: "timestamp without time zone",
+    default: () => "now()",
+  })
+  public created_at?: Date;
+
+  @ApiModelPropertyOptional({
+    description: "Timestamp representing user follow last update",
+    example: "2022-08-28 22:04:29.000000",
+  })
+  @UpdateDateColumn({
+    type: "timestamp without time zone",
+    default: () => "now()",
+  })
+  public updated_at?: Date;
+
+  @ApiHideProperty()
+  @DeleteDateColumn({
+    type: "timestamp without time zone",
+    select: false,
+  })
+  public deleted_at?: Date;
+}
