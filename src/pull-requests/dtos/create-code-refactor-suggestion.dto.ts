c5cb902dcdc3c3b08f3a6a8b323a253333c6c64c
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-05-22 20:57:18 +0530

    dto file formatting

--- src/pull-requests/dtos/create-code-refactor-suggestion.dto.ts
+++ src/pull-requests/dtos/create-code-refactor-suggestion.dto.ts
@@ -2,41 +2,41 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsIn, IsNumber, IsString, Max, Min } from "class-validator";
 
 export class GenerateCodeRefactorSuggestionDto {
-    @ApiProperty({
-      description: "Suggestion Length",
-      type: Number,
-      example: 250,
-    })
-      @IsNumber()
-      @Min(100)
-      @Max(500)
-      descriptionLength: number;
+  @ApiProperty({
+    description: "Suggestion Length",
+    type: Number,
+    example: 250,
+  })
+  @IsNumber()
+  @Min(100)
+  @Max(500)
+    descriptionLength: number;
 
 
-      @ApiProperty({
-        description: "Description Temperature",
-        type: Number,
-        example: 7,
-      })
-      @IsNumber()
-      @Min(0)
-      @Max(10)
-        temperature: number;
+  @ApiProperty({
+    description: "Description Temperature",
+    type: Number,
+    example: 7,
+  })
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+    temperature: number;
 
-      @ApiProperty({
-        description: "Suggestion Language",
-        type: String,
-        example: "english",
-        default: "english",
-      })
-      @IsString()
-      @IsIn(["english", "spanish", "french", "german", "italian", "portuguese", "dutch", "russian", "chinese", "korean"])
-        language: string;
+  @ApiProperty({
+    description: "Suggestion Language",
+    type: String,
+    example: "english",
+    default: "english",
+  })
+  @IsString()
+  @IsIn(["english", "spanish", "french", "german", "italian", "portuguese", "dutch", "russian", "chinese", "korean"])
+    language: string;
 
-        @ApiProperty({
-          description: "Code",
-          type: String,
-        })
-          @IsString()
-          code: string;
+  @ApiProperty({
+    description: "Code",
+    type: String,
+  })
+  @IsString()
+    code: string;
 }
