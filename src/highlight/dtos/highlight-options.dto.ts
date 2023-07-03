56eb3bdc5da31716c982af836807fa192a637e3c
El Hadji Malick Seck <takanomedev221@gmail.com>
2023-07-03 14:52:49 +0000

    fix: change swagger docs response for highlight reactions (#192)

--- src/highlight/dtos/highlight-options.dto.ts
+++ src/highlight/dtos/highlight-options.dto.ts
@@ -1,6 +1,7 @@
-import { ApiPropertyOptional } from "@nestjs/swagger";
+import { ApiPropertyOptional, PickType } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
 import { PageOptionsDto } from "../../common/dtos/page-options.dto";
+import { DbUserHighlightReaction } from "../../user/entities/user-highlight-reaction.entity";
 
 export class HighlightOptionsDto extends PageOptionsDto {
   @ApiPropertyOptional({
@@ -11,3 +12,8 @@ export class HighlightOptionsDto extends PageOptionsDto {
   @IsOptional()
   readonly repo?: string;
 }
+
+export class DbUserHighlightReactionResponse extends PickType(DbUserHighlightReaction, [
+  "emoji_id",
+  "reaction_count",
+]) {}
