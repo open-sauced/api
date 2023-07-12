06dcc2340fdbc5786dcf090d5b168ba3cbc89891
Brandon Roberts <robertsbt@gmail.com>
2023-07-12 10:52:57 -0500

    fix: add missing api service module imports

--- src/user-repo/user-repos.module.ts
+++ src/user-repo/user-repos.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { ApiServicesModule } from "../common/services/api-services.module";
 import { DbUserRepo } from "./user-repo.entity";
 import { UserReposService } from "./user-repos.service";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DbUserRepo], "ApiConnection")],
+  imports: [TypeOrmModule.forFeature([DbUserRepo], "ApiConnection"), ApiServicesModule],
   providers: [UserReposService],
   exports: [UserReposService],
 })
