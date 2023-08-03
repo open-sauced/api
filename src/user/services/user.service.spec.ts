0722ce42192dfc9cb192dfe7f1bb886894f6c562
Divyansh Singh <divyanshanandsingh@gmail.com>
2023-08-03 19:46:16 +0530

    fix tests

--- src/user/services/user.service.spec.ts
+++ src/user/services/user.service.spec.ts
@@ -58,6 +58,7 @@ describe("UserService", () => {
 
       const createQueryBuilderMock = {
         select: jest.fn().mockReturnThis(),
+        from: jest.fn().mockReturnThis(),
         innerJoin: jest.fn().mockReturnThis(),
         where: jest.fn().mockReturnThis(),
         groupBy: jest.fn().mockReturnThis(),
