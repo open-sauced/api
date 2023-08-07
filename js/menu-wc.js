34d2c7419e7150f58718c25d38f812d8c4aafacb
brandonroberts <brandonroberts@users.noreply.github.com>
2023-08-07 14:47:12 +0000

    Merge branch 'main' into beta 4e78760133e6b155a2a8d0f32a7dbc74f32bdd62

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">@open-sauced/api.opensauced.pizza documentation</a>
+                    <a href="index.html" data-type="index-link">@open-sauced/api documentation</a>
                 </li>
 
                 <li class="divider"></li>
