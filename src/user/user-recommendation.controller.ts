229551b4235faab8868ff04916771c92bb84f6c7
Brandon Roberts <robertsbt@gmail.com>
2023-08-30 10:02:03 -0500

    feat: add endpoint for recommended repos based on user organizations (#286)

--- src/user/user-recommendation.controller.ts
+++ src/user/user-recommendation.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
 import { ApiOperation, ApiBearerAuth, ApiTags, ApiOkResponse } from "@nestjs/swagger";
 
 import { SupabaseGuard } from "../auth/supabase.guard";
 import { UserId } from "../auth/supabase.user.decorator";
 
 import { RepoService } from "../repo/repo.service";
+import { PageOptionsDto } from "../common/dtos/page-options.dto";
+import { ApiPaginatedResponse } from "../common/decorators/api-paginated-response.decorator";
+import { DbRepo } from "../repo/entities/repo.entity";
 import { UserService } from "./services/user.service";
 
 @Controller("user/recommendations")
@@ -18,12 +21,26 @@ export class UserRecommendationController {
     summary: "Listing recommended repos for the authenticated user",
   })
   @ApiBearerAuth()
-  @ApiOkResponse()
+  @ApiPaginatedResponse(DbRepo)
+  @ApiOkResponse({ type: DbRepo })
   @UseGuards(SupabaseGuard)
   async findUserRepoRecommendations(@UserId() userId: number) {
     const user = await this.userService.findOneById(userId);
     const interests = user.interests?.split(",").filter(Boolean) ?? [];
 
     return this.repoService.findRecommendations(interests);
+  }
+
+  @Get("/orgs")
+  @ApiOperation({
+    operationId: "findUserOrgsRepoRecommendations",
+    summary: "Listing recommended repos for the authenticated user based on their orgs",
+  })
+  @ApiBearerAuth()
+  @ApiPaginatedResponse(DbRepo)
+  @ApiOkResponse({ type: DbRepo })
+  @UseGuards(SupabaseGuard)
+  async findUserOrgsRepoRecommendations(@UserId() userId: number, @Query() pageOptionsDto: PageOptionsDto) {
+    return this.repoService.findOrgsRecommendations(userId, pageOptionsDto);
   }
 }
