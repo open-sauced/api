92bd85c4132d0e5c885ca00f4bcefb4059c1ffb5
Sunday Ogbonna <oliviamegan11@gmail.com>
2023-09-04 16:12:02 +0100

    feat: add filtered users entity

---
+++ src/user/entities/filtered-users.entity.ts
@@ -0,0 +1,15 @@
+import { ApiModelProperty } from "@nestjs/swagger/dist/decorators/api-model-property.decorator";
+
+export class DbFilteredUser {
+  @ApiModelProperty({
+    description: " User Login",
+    example: "bdougie",
+  })
+  public login?: string;
+
+  @ApiModelProperty({
+    description: "Users fullname",
+    example: "Brian Douglas",
+  })
+  public full_name: string;
+}
