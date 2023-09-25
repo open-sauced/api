fa71fa513d9d103a057be80a48d3e14130d32dee
John McBride <john@opensauced.pizza>
2023-09-25 14:48:29 -0600

    feat: Enable multi select for location and timezone
    Signed-off-by: John McBride <john@opensauced.pizza>

--- src/user-lists/user-list.controller.ts
+++ src/user-lists/user-list.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards, ValidationPipe } from "@nestjs/common";
 import {
   ApiOperation,
   ApiOkResponse,
@@ -137,7 +137,9 @@ export class UserListController {
   @ApiOkResponse({ type: DbUser })
   @ApiNotFoundResponse({ description: "Unable to get contributors" })
   @ApiBadRequestResponse({ description: "Invalid request" })
-  async getContributors(@Query() pageOptionsDto: FilterListContributorsDto): Promise<PageDto<DbUser>> {
+  async getContributors(
+    @Query(new ValidationPipe({ transform: true, forbidUnknownValues: true })) pageOptionsDto: FilterListContributorsDto
+  ): Promise<PageDto<DbUser>> {
     return this.userListService.findContributorsByFilter(pageOptionsDto);
   }
 
