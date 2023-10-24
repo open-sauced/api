f9ced6ae0297b4c5c2de63c1ba07dab4b8600f81
John McBride <john@opensauced.pizza>
2023-10-24 09:05:45 -0600

    feat: Granular contributions for individual repos (#388)
    Signed-off-by: John McBride <john@opensauced.pizza>

--- src/contribution/contribution.service.ts
+++ src/contribution/contribution.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
+import { DbPullRequest } from "../pull-requests/entities/pull-request.entity";
 import { GetPrevDateISOString } from "../common/util/datetimes";
 import { PageDto } from "../common/dtos/page.dto";
 import { OrderDirectionEnum } from "../common/constants/order-direction.constant";
@@ -16,7 +17,9 @@ export class ContributionService {
     @InjectRepository(DbContribution, "ApiConnection")
     private contributionRepository: Repository<DbContribution>,
     @InjectRepository(DbUser, "ApiConnection")
-    private userRepository: Repository<DbUser>
+    private userRepository: Repository<DbUser>,
+    @InjectRepository(DbPullRequest, "ApiConnection")
+    private pullRequestRepository: Repository<DbPullRequest>
   ) {}
 
   async findAll(pageOptionsDto: ContributionPageOptionsDto, repoId?: number): Promise<PageDto<DbContribution>> {
@@ -83,5 +86,37 @@ export class ContributionService {
     }
 
     return entity;
+  }
+
+  async findAllByRepoId(
+    repoId: number,
+    range: number,
+    prev_days_start_date: number
+  ): Promise<DbRepoLoginContributions[]> {
+    const startDate = GetPrevDateISOString(prev_days_start_date);
+
+    const cteBuilder = this.pullRequestRepository
+      .createQueryBuilder("pull_requests")
+      .select("pull_requests.author_login", "login")
+      .addSelect("COALESCE(SUM(pull_requests.commits), 0)::INTEGER", "commits")
+      .addSelect("COUNT(pull_requests.id)::INTEGER", "prs_created")
+      .leftJoin("users", "users", "pull_requests.author_login = users.login")
+      .where(`pull_requests.repo_id = ${repoId}`)
+      .andWhere(`'${startDate}'::TIMESTAMP >= "pull_requests"."updated_at"`)
+      .andWhere(`'${startDate}'::TIMESTAMP - INTERVAL '${range} days' <= "pull_requests"."updated_at"`)
+      .andWhere("users.deleted_at IS NULL")
+      .groupBy("pull_requests.author_login");
+
+    const entitiesQb = this.userRepository.manager
+      .createQueryBuilder()
+      .addCommonTableExpression(cteBuilder, "CTE")
+      .setParameters(cteBuilder.getParameters())
+      .select("login")
+      .addSelect("commits")
+      .addSelect("prs_created")
+      .addSelect("(commits + prs_created)::INTEGER AS total_contributions")
+      .from("CTE", "CTE");
+
+    return entitiesQb.getRawMany();
   }
 }
