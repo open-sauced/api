b4a1511a27cdb099dc0e425c770b444bbf2dc51c
John McBride <john@opensauced.pizza>
2023-12-11 16:44:46 -0700

    feat: V2 pr search using timescale data
    - Switches relevant "where" clauses to use "LOWER" indexes
    
    Signed-off-by: John McBride <john@opensauced.pizza>

--- src/timescale/timescale.module.ts
+++ src/timescale/timescale.module.ts
@@ -1,10 +1,22 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { DbPullRequestGitHubEvents } from "./entities/pull_request_github_event";
+import { DbRepo } from "../repo/entities/repo.entity";
+import { DbUserList } from "../user-lists/entities/user-list.entity";
+import { DbUserListContributor } from "../user-lists/entities/user-list-contributor.entity";
+import { DbUserHighlight } from "../user/entities/user-highlight.entity";
+import { DbUser } from "../user/user.entity";
+import { RepoModule } from "../repo/repo.module";
+import { UserListModule } from "../user-lists/user-list.module";
 import { PullRequestGithubEventsService } from "./pull_request_github_events.service";
+import { DbPullRequestGitHubEvents } from "./entities/pull_request_github_event";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DbPullRequestGitHubEvents], "TimescaleConnection")],
+  imports: [
+    RepoModule,
+    UserListModule,
+    TypeOrmModule.forFeature([DbPullRequestGitHubEvents], "TimescaleConnection"),
+    TypeOrmModule.forFeature([DbRepo, DbUser, DbUserList, DbUserListContributor, DbUserHighlight], "ApiConnection"),
+  ],
   providers: [PullRequestGithubEventsService],
   exports: [PullRequestGithubEventsService],
 })
