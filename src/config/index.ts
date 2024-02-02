5d4a387520d8a176e88603301e13236318dc9d94
Brandon Roberts <robertsbt@gmail.com>
2024-02-02 09:47:10 -0600

    chore: remove tier config

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,4 @@ export * from "./endpoint.config";
 export * from "./stripe.config";
 export * from "./openai.config";
 export * from "./pizza.config";
-export * from "./tier.config";
 export * from "./timescale.config";
